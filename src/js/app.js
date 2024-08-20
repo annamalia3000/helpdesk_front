0976bf292b14e63c4c5ec9e3607a7c9d67529102
annamalia3000 <a.molch@mail.ru>
2024-08-20 17:48:14 +0300

    changed classes

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
-const tickets = document.querySelector('.tickets_list');
+const tickets = document.querySelector('.tickets-list');
 // Полное описание - показать и скрыть
 tickets.addEventListener('click', (e) => {
     const checkButton = e.target.closest('.check-btn');
 
     if (checkButton) {
-        const ticket = checkButton.closest('.ticket');
-        const fullDescription = ticket.querySelector('.ticket__desc__full');
+        const ticket = checkButton.closest('.ticket-item');
+        const fullDescription = ticket.querySelector('.ticket-details');
         if (checkButton.querySelector('.fa-check')) {
             checkButton.innerHTML = '';
             fullDescription.style.display = 'none';
@@ -17,20 +17,20 @@ tickets.addEventListener('click', (e) => {
 });
 //Добавить тикет - показать
 const addButton = document.querySelector('.add-btn');
-const popupAddTicket = document.querySelector('.popup-add-ticket');
+const popupAddTicket = document.querySelector('.popup-add');
 
 addButton.addEventListener('click', () => {
     popupAddTicket.style.display = 'block';
 });
 //Добавить тикет - скрыть
-const popupCancelButton = document.querySelector('.popup-add-ticket__btn-cancel');
+const popupCancelButton = document.querySelector('.popup-add-btn-cancel');
 
 popupCancelButton.addEventListener('click', () => {
     popupAddTicket.style.display = 'none';
 });
 
 //Исправить тикет -  показать
-const popupEditTicket = document.querySelector('.popup-edit-ticket');
+const popupEditTicket = document.querySelector('.popup-edit');
 tickets.addEventListener('click', (e) => {
     const editButton = e.target.closest('.edit-btn');
 
